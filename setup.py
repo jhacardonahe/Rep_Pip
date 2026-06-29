a5516272be6da9651d5dc38fd5894b7c273abcd5
jhacardonahe <jhacardonahe@gmail.com>
2026-06-29 17:09:06 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="REP_Pip",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Jhacardonahe",                         # Tu nombre
+    author_email="ensayolio@platzilio.com",                 # Tu correo electrónico
+    url="https://github.com/jhacardonahe/Rep_Pip",     # URL del proyecto
+)
